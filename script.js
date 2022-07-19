b6a4550928e678aacf8feca805ecca284e974734
Ghate Suyog <suyogghateminks@gmail.com>
2022-07-19 23:30:42 +0530

    Completed

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 //your code here
+function getRandomNumber(min, max){
+    return Math.floor(Math.random() * (max - min)) + min;
+} 
+
+let btn = document.getElementById('btn');
+
+btn.addEventListener('click', (e) => {
+    let response = document.getElementById('respond');
+
+    let randomNumber = getRandomNumber(-20, 20);
+    let num = document.getElementById('num');
+    num.innerText = randomNumber;
+
+    let userGuess = document.getElementById('guess');
+    
+    let difference = Math.abs(userGuess.value - randomNumber);
+
+    if(difference >= 0 && difference <= 3){
+        response.innerText = "HOT";
+    }
+    else{
+        response.innerText = "COLD";
+    }
+    
+})
 
